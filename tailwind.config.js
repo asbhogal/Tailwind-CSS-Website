deaaa0cf8920895fb4e909e4619e23930f79a3e0
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-17 18:21:12 +0100

    installed Vercel CLI (local) & changed dirs in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,css}'],
+  content: ['./src/**/*.{html,js,css}', '. {html}'],
   theme: {
     screens: {
       sm: '480px',
