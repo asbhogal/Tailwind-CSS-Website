60c3426a180bce4d14f0889bd75b05a598d658a9
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-17 15:10:09 +0100

    added 'screens' breakpoints to tailwing config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,css}'],
   theme: {
+    screens {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
