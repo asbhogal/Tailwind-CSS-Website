29f2868c2c8269baded835864f8516f835599245
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-17 15:59:59 +0100

    resolved config file syntax & extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{html,js,css}'],
   theme: {
-    screens {
-      sm: '480px';
-      md: '768px';
-      lg: '976px';
-      xl: '1440px';
-    };
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
-        brightRed             :   '#f25f3a';
-        brightRedLight        :   '#f6866a';
-        brightRedSuperLight   :   '#fdece7';
-        darkBlue              :   '#242d52';
-        darkGrayishBlue       :   '#9095a7';
-        veryDarkBlue          :   '#1d1e25';
-        veryPaleRed           :   '#ffefeb';
-        veryLightGray         :   '#fafafa';
-      };
-    };
-  };
+        brightRed             :   '#f25f3a',
+        brightRedLight        :   '#f6866a',
+        brightRedSuperLight   :   '#fdece7',
+        darkBlue              :   '#242d52',
+        darkGrayishBlue       :   '#9095a7',
+        veryDarkBlue          :   '#1d1e25',
+        veryPaleRed           :   '#ffefeb',
+        veryLightGray         :   '#fafafa',
+      }
+    }
+  },
   plugins: [],
 }
