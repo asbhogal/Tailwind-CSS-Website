6897d6af75be2daae4fe81398b226e11912fd56b
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-19 19:45:22 +0100

    added script for email validation in newsletter sign up input

---
+++ src/script.js
@@ -0,0 +1,45 @@
+// EMAIL VALIDATION ON A PRODUCTION BUILD SHOULD ALWAYS BE PERFORMED ON SERVER-SIDE AS WELL AS CLIENT-SIDE (IN CASE BROWSER HAS TURNED JAVASCRIPT OFF). FOR THIS PROJECT, JAVASCRIPT IS LEVERAGED FOR LOCAL VALIDATION ONLY.
+
+const 	emailAddressInput = document.getElementById('email-address-input'),
+		emailAddressSubmitBtn = document.getElementById('submit-email-address'),
+		responseMessage = document.getElementById('response-message'),
+		regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// FUNCTION WHICH CHECKS IF AN EMAIL IS ENTERED 
+
+function checkInputPresent (email) {
+	return (!emailAddressInput.value) ? (responseMessage.innerHTML = "Please enter an email address", emailAddressInput.style.borderColor = "red", emailAddressInput.style.outlineColor = "red") 
+		: toLowerCase(email);
+}
+
+// FUNCTION WHICH CONVERTS ALL THE CHARACTERS IN THE EMAIL ADDRESS --> LOWER CASE
+
+function toLowerCase () {
+	convertedEmail = String(emailAddressInput.value).toLowerCase();
+	validateEmail();
+}
+
+// FUNCTION WHICH VALIDATES THE EMAIL ENTERED
+
+function validateEmail() {
+	return (regex.test(emailAddressInput.value) == true) ? (responseMessage.innerHTML = "Thank you for signing up!", emailAddressInput.value = "", emailAddressInput.style.borderColor = "green", emailAddressInput.style.outlineColor = "green")
+		: (responseMessage.innerHTML = "Please insert a valid email", emailAddressInput.style.borderColor = "red", emailAddressInput.style.outlineColor = "red")
+}
+
+// TRIGGER WHICH CALLS THE FIRST FUNCTION ON 'SUBMIT' BUTTON CLICK
+
+emailAddressSubmitBtn.addEventListener('click', () => {
+	email = emailAddressInput;
+	checkInputPresent(email);
+	return responseMessage;
+});
+
+// TRIGGER WHICH CALLS THE FIRST FUNCTION ON 'ENTER' KEY IN INPUT
+
+emailAddressInput.onkeydown = function(e) {
+	if(e.keyCode == 13) {
+		email = emailAddressInput;
+		checkInputPresent(email);
+		return responseMessage;
+	}
+}
