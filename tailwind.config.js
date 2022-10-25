7f696d2ca975c4934327b1cc9af831817b09dffc
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-25 19:14:28 +0100

    changes to responsive widths in tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./src/**/*.{js,css}', './*.html'],
   theme: {
     screens: {
-      sm: '480px',
+      sm: '375px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
