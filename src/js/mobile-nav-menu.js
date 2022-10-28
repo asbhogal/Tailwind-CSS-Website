0e4c11bff63c18b17626d65594e7b244dd652e61
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-28 22:57:14 +0100

    replaced static hamburger svg with span elements

--- src/js/mobile-nav-menu.js
+++ src/js/mobile-nav-menu.js
@@ -2,6 +2,7 @@ const   mobileMenu = document.getElementById('mobile-nav-menu'),
         mobileBtn = document.getElementById('mobile-nav-btn');
 
 mobileBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('flex')
-    mobileMenu.classList.toggle('hidden')
+    mobileBtn.classList.toggle('open');
+    mobileMenu.classList.toggle('flex');
+    mobileMenu.classList.toggle('hidden');
 });
