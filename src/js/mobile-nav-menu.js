9da2566fc39793ab9492c6b76956c898b2218809
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-28 20:33:06 +0100

    further adjustments to element positioning and styling

--- src/js/mobile-nav-menu.js
+++ src/js/mobile-nav-menu.js
@@ -1,9 +1,7 @@
 const   mobileMenu = document.getElementById('mobile-nav-menu'),
         mobileBtn = document.getElementById('mobile-nav-btn');
 
-window.onload = () => {
-    mobileBtn.addEventListener('click', () => {
-        mobileMenu.classList.toggle('flex');
-        mobileMenu.classList.toggle('hidden');
-    })
-}
+mobileBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('flex');
+    mobileMenu.classList.toggle('hidden');
+});
