3bd415584b2ce56f7a4422da0d13cbbc323df3b9
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-28 13:43:19 +0100

    added swiper js testimonial slider and alt text for images

--- src/script.js
+++ src/script.js
@@ -43,3 +43,44 @@ emailAddressInput.onkeydown = function(e) {
 		return responseMessage;
 	}
 }
+
+// TESTIMONIAL SLIDER
+
+let swiper = new Swiper(".testimonial-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: false,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        dynamicBullets: true,
+        clickable: true,
+        dynamicMainBullets: 4,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        375: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        976: {
+            slidesPerView: 3,
+        },
+        1440: {
+            slidesPerView: 4
+        }
+    }
+});
