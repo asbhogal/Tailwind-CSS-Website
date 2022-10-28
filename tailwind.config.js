52e701ac958fb4959f268d6dbbadad860dc3f76e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-28 19:58:45 +0100

    further adjustments to styling and positioning

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,9 @@ module.exports = {
         veryPaleRed           :   '#ffefeb',
         veryLightGray         :   '#fafafa',
       },
+      height: {
+        250: '250px',
+      }
     }
   },
   plugins: [],
